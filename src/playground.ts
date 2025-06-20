a1d15d86554624b6ad690714a18cff8e99e168e1
anish1A1 <alloozpat87@gmail.com>
2025-06-20 18:05:41 +0545

    commented the playground check for now

--- src/playground.ts
+++ src/playground.ts
@@ -1,11 +1,11 @@
-import { db } from "./server/db";
+// import { db } from "./server/db";
 
-await db.user.create({
-    data:{
-        emailAddress: 'test@gmail.com',
-        firstName: 'Anish',
-        lastName : 'Jas', 
-    }
-})
+// await db.user.create({
+//     data:{
+//         emailAddress: 'test@gmail.com',
+//         firstName: 'Anish',
+//         lastName : 'Jas', 
+//     }
+// })
 
-console.log('done')
+// console.log('done')
