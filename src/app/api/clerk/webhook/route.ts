fa0aaab78cc08468d8b8dc991fbb6616c82ad5f1
anish1A1 <alloozpat87@gmail.com>
2025-06-26 19:57:51 +0545

    disabled eslint and added route file to receive clerk webhook endpoints

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,10 @@
+// api/clerk/webhook
+
+
+export const POST = async (req: Request) => {
+    const {data} = await req.json()
+    console.log('Clerk Webhook received', data)
+
+    return new Response("Webhook Received", {status: 200})
+}
+
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
+  // if (typeof window !== 'undefined')
   return (
     <div className="flex justify-center items-center h-screen">
         <SignIn />
